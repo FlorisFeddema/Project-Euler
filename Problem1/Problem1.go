f7c9e12f1d7169fc728d13b76b58252433a91452
Floris Feddema <floris1996@hotmail.com>
2020-11-22 18:01:13 +0100

    Problem1

---
+++ Problem1/Problem1.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sum := 0
+
+	for i := 0; i < 1000; i++ {
+		if i % 3 == 0 || i % 5 == 0 {
+			sum += i
+		}		
+	}
+	
+	fmt.Printf("Result: %[1]d", sum)
+}
